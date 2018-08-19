3e0aa0ebc7537c6a38cf739c7a0ec438ac396ad0
Andrew Darts <andrew@spurjobs.com>
2018-08-19 10:29:49 -0500

    added markdown support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { TransferHttpCacheModule } from '@nguniversal/common';
+import { MarkdownModule } from 'ngx-markdown';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'my-app' }),
     AppRoutingModule,
+    HttpClientModule,
     TransferHttpCacheModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/posts/test.md
@@ -0,0 +1,4 @@
+# Test Post
+> Hell Yeah!
+
+No way __aweomse__
